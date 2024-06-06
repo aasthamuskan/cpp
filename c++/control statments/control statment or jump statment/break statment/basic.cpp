7026a559469c3774b38c1bae986b73602d754881
aasthamuskan9430@gmail.com <aasthamuskan9430@gmail.com>
2024-06-06 17:29:52 +0530

    day 6

---
+++ c++/control statments/control statment or jump statment/break statment/basic.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i;
+    i=1;
+    while (i<=5){
+        cout<<endl<<i;
+        if(i==30)
+        break;
+        i++;
+    }
+    return 0;
+}
