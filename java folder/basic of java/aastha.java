c044e27ae6e359b1ba1bef366fefd3a4e64a75f2
aasthamuskan9430@gmail.com <aasthamuskan9430@gmail.com>
2025-01-10 18:29:50 +0530

    day 9,10

--- java folder/basic of java/aastha.java
+++ java folder/basic of java/aastha.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.Scanner;
 public class aastha{
     public static void main(String[ ] args){
         System.out.println("radhe radhe");
@@ -161,25 +162,204 @@ public class aastha{
          //12. modulo operator
             System.out.println(64%5);
 
-            
+        //maths
+        System.out.println(Math.max(5,6));
+        System.out.println(Math.min(5,6));
+        System.out.println(Math.sqrt(64));
+        System.out.println(Math.abs(-64));
+        System.out.println(Math.random());
+        System.out.println(Math.random()*100);
+        System.out.println((int)(Math.random()*100));
+        System.out.println(Math.round(5.6));
+        System.out.println(Math.ceil(5.6));
+        System.out.println(Math.floor(5.6));
+        System.out.println(Math.pow(5,2));
+        System.out.println(Math.PI);
+        System.out.println(Math.E);
 
-            
+        //how to take input
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number");
+        int num=sc.nextInt();
+        System.out.println(num);
+        System.out.println("Enter the string");
+        String str=sc.next();
+        System.out.println(str);
+        String name = sc.nextLine();//full line
+        System.out.println(name);
 
+        System.out.println("Enter the float");
+        float num1=sc.nextFloat();
+        System.out.println(num1);
+        System.out.println("Enter the double");
+        double num2=sc.nextDouble();
+        System.out.println(num2);
+        System.out.println("Enter the boolean");
+        boolean num3=sc.nextBoolean();
+        System.out.println(num3);
+        System.out.println("Enter the character");
+        char num4=sc.next().charAt(0);
+        System.out.println(num4);
 
-
-
-
-
-
-
-
-
-
+        //if else
+        int age=20;
+        if(age>18){
+            System.out.println("You can drive");
+        }
+        else{
+            System.out.println("You cannot drive");
+        }
         
+        //logical operators
+        int age=20;
+        if(age>18 && age<60){
+            System.out.println("You can drive");
+        }
+        else{
+            System.out.println("You cannot drive");
+        }
 
+        //switch case
+        int day=4;
+        switch(day){
+            case 1:
+            System.out.println("Monday");
+            break;
+            case 2:
+            System.out.println("Tuesday");
+            break;
+            case 3:
+            System.out.println("Wednesday");
+            break;
+            case 4:
+            System.out.println("Thursday");
+            break;
+            case 5:
+            System.out.println("Friday");
+            break;
+            case 6:
+            System.out.println("Saturday");
+            break;
+            case 7:
+            System.out.println("Sunday");
+            break;
+            default:
+            System.out.println("Invalid day");
+        }
 
+        //loops
+        //1. for loop
+        for(int i=0;i<5;i++){
+            System.out.println(i);
+        }
+
+        //2. while loop
+        int i=0;
+        while(i<5){
+            System.out.println(i);
+            i++;
+        }
+
+        //3. do while loop
+        int i=0;
+        do{
+            System.out.println(i);
+            i++;
+        }while(i<5);
+
+        //4. break statement
+        for(int i=0;i<5;i++){
+            if(i==3){
+                break;
+            }
+            System.out.println(i);
+        }
+
+        //5. continue statement
+        for(int i=0;i<5;i++){
+            if(i==3){
+                continue;
+            }
+            System.out.println(i);
+        }
+
+        //6. nested loop
+        for(int i=0;i<5;i++){
+            for(int j=0;j<5;j++){
+                System.out.println(i+" "+j);
+            }
+        }
+
+        //7. enhanced for loop
+        int [] marks={1,2,3,4,5};
+        for(int i:marks){
+            System.out.println(i);
+        }
+
+        //8. infinite loop
+                for(int i=0;i<5;){
+                    System.out.println(i);
+                }
         
+    
+//do while  problem  1 by only taking positive input
+
+Scanner sc= new Scanner(System.in);
+System.out.println("Enter the number");
+int num=sc.nextInt();
+do{
+    System.out.println("Enter the number");
+    num=sc.nextInt();   
+}while(num>=0);
+System.out.println("Negative number entered");  
 
 
-    }
+//exception handling
+try{
+    int [] marks={1,2,3,4,5};
+    System.out.println(marks[7]);
 }
+catch(Exception e){
+    System.out.println("Invalid index");
+}
+finally{
+    System.out.println("Thank you");
+}
+
+//try with resources
+try(Scanner sc=new Scanner(System.in)){
+    int [] marks={1,2,3,4,5};
+    System.out.println(marks[7]);
+}
+catch(Exception e){
+    System.out.println("Invalid index");
+}
+finally{
+    System.out.println("Thank you");
+}
+
+
+//method
+public static void greet(){
+    System.out.println("Good Morning");
+}
+public static void greet(String name){
+    System.out.println("Good Morning "+name);
+}
+public static int sum(int a,int b){
+    return a+b;
+}
+public static void main(String[ ] args){
+    greet();
+    greet("Aastha");
+    System.out.println(sum(5,6));
+}
+public static void printsum(int a,int b){
+    System.out.println("The sum is "+(a+b));
+}
+
+
+
+
+
+
